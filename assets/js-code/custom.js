5f4670e43c8d7aa4276c12907631a430ad46dfbc
coder-suzan <79319220+coder-suzan@users.noreply.github.com>
2022-07-20 23:39:19 +0600

    Add files via upload

---
+++ assets/js-code/custom.js
@@ -0,0 +1,24 @@
+// Add active class to the menulink for showing the button active color
+var header = document.getElementById("activeMenu");
+var btns = header.getElementsByClassName("menuLink");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
+
+
+// mobile menu
+function openNav() {
+ var mobileMenu = document.getElementById("mobileMenuShow");
+  mobileMenu.style.width = "240px";
+  mobileMenu.style.display = "block";
+}
+
+function closeNav() {
+  var mobileMenu = document.getElementById("mobileMenuShow");
+    mobileMenu.style.width = "0";
+    mobileMenu.style.display = "none";
+}
